6a0c8a4f6fd3e65d9bda1d86ab3b17a1d6653710
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-11-18 16:31:17 -0500

    zink: add compute-based texture clears
    based on radeonsi implementation

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -142,6 +142,11 @@ zink_context_destroy(struct pipe_context *pctx)
    hash_table_foreach(&ctx->framebuffer_cache, he)
       zink_destroy_framebuffer(screen, he->data);
 
+   if (ctx->cs_clear_render_target)
+      pctx->delete_compute_state(pctx, ctx->cs_clear_render_target);
+   if (ctx->cs_clear_render_target_1d_array)
+      pctx->delete_compute_state(pctx, ctx->cs_clear_render_target_1d_array);
+
    hash_table_foreach(ctx->render_pass_cache, he)
       zink_destroy_render_pass(screen, he->data);
 
@@ -4424,4 +4429,22 @@ fail:
    if (ctx)
       zink_context_destroy(&ctx->base);
    return NULL;
+}
+
+void
+zink_compute_internal(struct zink_context *ctx, struct pipe_grid_info *info, void *shader, bool render_condition)
+{
+   /* Dispatch compute. */
+   void *saved_cs = ctx->compute_stage;
+   bool render_condition_active = ctx->render_condition_active;
+   if (!render_condition && render_condition_active)
+      zink_stop_conditional_render(ctx);
+   ctx->render_condition_active = false;
+   ctx->base.bind_compute_state(&ctx->base, shader);
+   ctx->base.launch_grid(&ctx->base, info);
+   ctx->base.bind_compute_state(&ctx->base, saved_cs);
+   ctx->render_condition_active = render_condition_active;
+
+   if (!render_condition && ctx->render_condition_active)
+      zink_start_conditional_render(ctx);
 }
