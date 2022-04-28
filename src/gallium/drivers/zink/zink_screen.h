19d5554c6957408355cdbcfa3c82d03b1c33e665
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-04-28 16:50:56 -0400

    zink: remove drm_fd
    no longer used

--- src/gallium/drivers/zink/zink_screen.h
+++ src/gallium/drivers/zink/zink_screen.h
@@ -97,7 +97,6 @@ struct zink_screen {
    simple_mtx_t dt_lock;
 
    bool device_lost;
-   int drm_fd;
    struct sw_winsys winsys;
    struct sw_winsys *sw_winsys; // wrapped
    __DRIkopperLoaderExtension *loader;
